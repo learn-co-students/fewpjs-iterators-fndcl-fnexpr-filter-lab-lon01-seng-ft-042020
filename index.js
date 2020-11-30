40362339d84761920235d38d0905f0a4870a9e9d
thealekid <chukorakwusi@gmail.com>
2020-11-30 19:56:33 +0000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function findMatching(drivers, string){
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+};
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => driver.toLowerCase().indexOf(string.toLowerCase()) === 0 )
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string)
+}
 
